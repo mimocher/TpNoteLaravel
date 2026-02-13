f5b3e52f399bc1ab2fd5c569fc88d364f9970565
Maroua Cherki <cherkimaroua7@gmail.com>
2026-02-13 13:12:47 +0100

    Add files via upload

---
+++ ClientSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Client;
+use Illuminate\Database\Seeder;
+
+class ClientSeeder extends Seeder
+{
+    public function run()
+    {
+        Client::factory()->count(5)->create();
+    }
+}
